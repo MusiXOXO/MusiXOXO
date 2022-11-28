57c824b7c87cfd9decd898370651478e58a597a1
Jakub Zieliński <jakub.zieli02@gmail.com>
2022-11-28 19:59:13 +0100

    Added 2 functions: 'get_auth_key' 'get_artist_song'

--- EasyspotifyAPI.py
+++ EasyspotifyAPI.py
@@ -1,3 +1,19 @@
 import json
+def get_auth_key():
+    with open('AuthKey.txt') as read_file:
+        return read_file.read()
 
-print("testxd")
+def get_artist_song( user_string ):
+    __artist_song = ['','']  #first goes artist then song name
+    
+    #find if its artist - song or just song || also remove any spaces from front and back 
+    if "-" in user_string:
+        __artist_song[0] = user_string[0: user_string.index('-') : 1].strip()
+        __artist_song[1] = user_string[user_string.index('-')+1 :  len(user_string)+1 : 1].strip()
+    else:
+        __artist_song[1]=user_string
+
+    #Get rid of needless 'spaces'
+    __artist_song[0].strip()
+
+    return __artist_song
